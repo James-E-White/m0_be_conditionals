cd1e123406f1ebc96a847f4ec6081e9388329300
James White <whitejaad@gmail.com>
2022-07-11 14:39:41 -0700

    Initial commit

--- comparisons.rb
+++ comparisons.rb
@@ -22,24 +22,31 @@ puts "Is number_teachers greater than number_students?", number_teachers > numbe
 # this should print: "Is numberTeachers greater than numberStudents?" false
 
 # YOU DO: print the result of the comparison: is number_teachers less than number_students?
+puts "Is the number_teachers less than number_Students?", number_teachers > number_students
 # this should print: true
 
 # YOU DO: print the result of the comparison: is number_teachers equal to string_teachers?
+puts "Is number_teachers equal to string_teachers", number_teachers == string_teachers
 # this should print: false
 
 # YOU DO: print the result of the comparison: is number_teachers not equal to number_students?
+puts "Is number_teachers not equal to number_students?", number_teachers != number_students
 # this should print: true
 
 # YOU DO: print the result of the comparison: is number_students greater than or equal to 20?
+puts "is number_students greater than or equal to 20?", number_students >= 20
 # this should print: true
 
 # YOU DO: print the result of the comparison: is number_students greater than or equal to 21?
+puts "is number_students greater than or equal to 21?", number_students >= 21
 # this should print: false
 
 # YOU DO: print the result of the comparison: is number_students less than or equal to 20?
+puts "is number_students less than or equal to 20?", number_students <= 20
 # this should print: true
 
 # YOU DO: print the result of the comparison: is number_students less than or equal to 21?
+puts "is number_students less than or equal to 21?", number_students <= 21
 # this should print: true
 
 
@@ -53,20 +60,26 @@ puts "Is number_teachers greater than number_students?", number_teachers > numbe
 # Make sure YOU can explain it that way!
 
 puts 4 < 9
+#prints the comparison of the intergers 4 and 9. the < denotes that the statement should be true if 9 is greater
+#4
 #YOU DO: Explain.
 
 books = 3
 puts 4 < books
+#prints the comparison of data type book =3 and the interger 4. The statement will be false as books=3 and the other
+#interger is greater 4
 # YOU DO: Explain.
 
 friends = 6
 siblings = 2
 puts friends > siblings
+# Compares the array of friends/siblings. The statement will be true as 6 is greater than 2.
 # YOU DO: Explain.
 
 attendees = 9
 meals = 8
 puts attendees != meals
+#prints the attendees 9 is not equal to meals 9. Statement is true
 # YOU DO: Explain.
 
 
@@ -90,15 +103,15 @@ age = 1
 
 # YOU DO:
 # Determine if the dog loves to play and loves treats
-
+puts "loves_to_play and loves_treats", loves_to_play && loves_treats
 
 # Determine if the dog loves to play and loves the dog park
-
+puts "loves_to_play and loves_dog_park", loves_to_play && loves_dog_park
 
 # Determine if the dog loves to play or loves the dog park
-
+puts loves_to_play || loves_dog_park
 
 # Determine if the dog loves to play and is a puppy
-
+puts loves_to_play && age
 # What did your final line of code evaluate to? Why do you think that is? Explain.
-# ANSWER:
+# ANSWER: It is evaluating the age of the dog not what a puppy is :)
--- decision_making.rb
+++ decision_making.rb
@@ -4,8 +4,8 @@
 
 
 door_choice = 1
-bear_clothing = ""
-bear_choice = 1
+bear_clothing = "hat"
+bear_choice = 2
 
 puts "You enter a dark room with two doors. Do you go through #1 or #2?"
 
@@ -22,22 +22,29 @@ puts "1. Offer your own to the bear?"
 puts "2. Point it out to the bear?"
 puts "3. Make a dash for the next room?"
 
+
 if bear_choice == 1
-  "You offer the bear your " + bear_clothing + " and the bear shows you a secret passage out!"
+puts  "You offer the bear your " + bear_clothing + " and the bear shows you a secret passage out!"
 elsif bear_choice == 2
-  "You tell the bear the " + bear_clothing + " is too small and it starts to cry!"
+puts  "You tell the bear the " + bear_clothing + " is too small and it starts to cry!"
 elsif bear_choice == 3
-  "You run as fast as you can into the next room. It's full of snakes!"
+puts  "You run as fast as you can into the next room. It's full of snakes!"
 else
-  "You stay with the bear and become it's best friend!"
+puts  "You stay with the bear and become it's best friend!"
 end
 
 
 # Questions
 # 1. In English, using technical vocabulary, describe what is happening between lines 12 and 16.
+# you are defining what happens with bear choice. Giving value to item and its assignment
 # 2. What variable has a new value assigned to it after the first if statement executes?
+#the variable is the piece of clothing
 # 3. If you changed the variable door_choice to equal 3, what would the bear_clothing value be?
+#the bear_clothing will always be scarf with any number value besides 1
 # 4. In English, using technical vocabulary, describe what is happening between lines lines 25 and 33.
+# You are defining the outcome of the number choice. giving outcomes based on the intergers.
 # 5. If you changed the variable bear_choice to equal 3, what will be the final outcome be?
+# you run as fast as you can into the next room. It's full of snakes!
 # 6. If you changed the variable door_choice to equal 1, and the variable bear_choice to equal 2, what will be the final outcome be?
-# 7. What is your favorite ending?
+# You tell the bear the hat is too small and it starts to cry
+# 7. What is your favorite ending? "You stay with the bear and become it's best friend!"
